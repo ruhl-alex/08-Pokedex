3303e52267ab7f4ab0ad13db664bcf0de0ce5b70
Alexander Ruhl <ruhl-alex@web.de>
2026-02-12 12:31:05 +0100

    Update 12.02.2026

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ let BASE_URL = "https://pokeapi.co/api/v2/";
 
 async function onload() {
     let pokesList = await loadDataFromApi("pokemon?limit=25&offset=0");
+    
     for (let index = 0; index < pokesList.results.length; index++) {
+        let pokesList2 = await loadDataFromApi2("pokemon/" + (index + 1))
         pokes.push(
             {
                 id: index + 1,
                 name: pokesList.results[index].name,
-                url: pokesList.results[index].url
+                url: pokesList.results[index].url,
+                // img: pokesList2.results[index].spirites.other.official-artwork
             }
         );}
     console.log(pokes);
@@ -17,5 +20,10 @@ async function onload() {
 
 async function loadDataFromApi(path = "") {
     let response = await fetch(BASE_URL + path + ".json");
+    return responseToJson = await response.json();
+}
+
+async function loadDataFromApi2(path = "") {
+    let response = await fetch(BASE_URL + path);
     return responseToJson = await response.json();
 }

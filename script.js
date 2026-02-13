77fb1442f97855d68c2edd7652fc41227fbacf4d
Alexander Ruhl <ruhl-alex@web.de>
2026-02-13 21:37:14 +0100

    update13.02.2026

--- script.js
+++ script.js
@@ -1,29 +1,59 @@
 let pokes = [];
-
 let BASE_URL = "https://pokeapi.co/api/v2/";
+let limit = 20;
+let offSet = 0;
 
 async function onload() {
-    let pokesList = await loadDataFromApi("pokemon?limit=25&offset=0");
-    
-    for (let index = 0; index < pokesList.results.length; index++) {
-        let pokesList2 = await loadDataFromApi2("pokemon/" + (index + 1))
-        pokes.push(
-            {
-                id: index + 1,
-                name: pokesList.results[index].name,
-                url: pokesList.results[index].url,
-                // img: pokesList2.results[index].spirites.other.official-artwork
-            }
-        );}
+    await loadPokes();
+    await loadImgAndTypes();
+    renderPokes();
     console.log(pokes);
 }
 
+async function loadPokes() {
+    let pokesList = await loadDataFromApi("pokemon?limit=${limit}&offset=${offSet}");
+    for (let index = 0; index < pokesList.results.length; index++) {
+
+        pokes.push(
+            {
+                id: index + 1,
+                name: pokesList.results[index].name.charAt(0).toUpperCase() + pokesList.results[index].name.slice(1),
+                url: pokesList.results[index].url,
+            }
+        );
+    }
+}
+
+async function loadImgAndTypes() {
+    for (let index = 0; index < pokes.length; index++) {
+        let pokesList2 = await loadDataFromApi("pokemon/" + (index + 1))
+        let typesArray = [];
+
+        pokes[index].img = pokesList2.sprites.other["official-artwork"].front_default;
+
+        for (let i = 0; i < pokesList2.types.length; i++) {
+            typesArray.push(pokesList2.types[i].type.name.charAt(0).toUpperCase() + pokesList2.types[i].type.name.slice(1));
+        }
+        pokes[index].type = typesArray;
+    }
+}
+
 async function loadDataFromApi(path = "") {
-    let response = await fetch(BASE_URL + path + ".json");
+    let response = await fetch(BASE_URL + path);
     return responseToJson = await response.json();
 }
 
-async function loadDataFromApi2(path = "") {
-    let response = await fetch(BASE_URL + path);
-    return responseToJson = await response.json();
+function renderTypes(types) {
+    let typesHTML = "";
+    for (let i = 0; i < types.length; i++) {
+        typesHTML += `<span class="poke-type-span">${types[i]}</span>`;
+    }
+    return typesHTML;
+}
+
+async function loadMorePokes() {
+    offSet += limit;
+    await loadPokes();
+    await loadImgAndTypes();
+    renderPokes();
 }
---
+++ script/template.js
@@ -0,0 +1,20 @@
+function renderPokes() {
+    const pokeContainer = document.getElementById("poke-container");
+    pokeContainer.innerHTML = "";
+
+    for (let index = 0; index < pokes.length; index++) {
+        pokeContainer.innerHTML +=  `
+            <div class="poke-card">
+                <div id="id-poke-card-name" class="poke-card-name">
+                    ${pokes[index].name}
+                </div>
+                <div id="id-poke-card-img" class="poke-card-img">
+                    <img class="poke-img" src="${pokes[index].img}" alt="${pokes[index].name}">
+                </div>
+                <div id="id-poke-card-art" class="poke-card-art">
+                    ${renderTypes(pokes[index].type)}
+                </div>
+            </div>
+        `
+}}
+
